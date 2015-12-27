87cae79c5d468a94c1a47a25a3d3f08884b6748f
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-12-27 15:45:08 -0500

    fix

--- src/app/steps/writing.js
+++ src/app/steps/writing.js
@@ -9,6 +9,7 @@ export default {
   serverDependentApi () {
     this.fs.copy(this.templatePath('connection/**'), this.destinationPath('connection'));
     this.fs.copy(this.templatePath('routes/**'), this.destinationPath('routes'));
+    this.fs.copy(this.templatePath('models/**'), this.destinationPath('models'));
   },
 
   config () {
@@ -37,13 +38,17 @@ export default {
   },
 
   development () {
+    this.fs.copy(this.templatePath('eslintrc'), this.destinationPath('.eslintrc'));
+    this.fs.copy(this.templatePath('eslintignore'), this.destinationPath('.eslintignore'));
     this.fs.copy(this.templatePath('editorconfig'), this.destinationPath('.editorconfig'));
     this.fs.copy(this.templatePath('gitignore'), this.destinationPath('.gitignore'));
+    this.fs.copy(this.templatePath('README.md'), this.destinationPath('README.md'));
   },
 
   pkg () {
     // node:app generator will merge into this
-    let trailsPackage = require(this.templatePath('package.json'));
-    this.fs.writeJSON(this.destinationPath('package.json'), trailsPackage);
+    let package = require(this.templatePath('package.json'));
+    package.name = this.options.name;
+    this.fs.writeJSON(this.destinationPath('package.json'), package);
   }
 };
