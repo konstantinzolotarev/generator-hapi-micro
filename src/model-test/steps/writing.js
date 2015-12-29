fcd8fa21535988b58ce4a4f84f39b4ed89fe61da
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-12-29 00:28:23 -0500

    Working

--- src/model-test/steps/writing.js
+++ src/model-test/steps/writing.js
@@ -3,7 +3,6 @@
  * Where you write the generator specific files (routes, controllers, etc)
  */
 
-// import Util from '../../app/util'
 
 const SOURCE_SMOKE_MODEL = `Model.smoke.js`;
 const SOURCE_INTEGRATION_MODEL = `Model.integration.js`;
@@ -14,15 +13,13 @@ const DESTINATION_MODEL_INTEGRATION = name => `test/integration/models/${name}.t
 const DESTINATION_MODEL_HELPER = name => `test/helper/model/${name}.js`;
 
 export default function () {
-  // Util.patchConflicter()
 
   let name = (this['model-name'].charAt(0).toUpperCase() + this['model-name'].slice(1)).replace(/(\w+)Model$/, '$1');
   let fileName = `${name}`
-  let indexPath = this.destinationPath(DESTINATION_MODEL_SMOKE)
 
   this.template(SOURCE_SMOKE_MODEL, DESTINATION_MODEL_SMOKE(name), {name, fileName, answers: this.answers});
 
-  if (!this.fs.exists(this.destinationPath(DESTINATION_MODEL_SMOKE))) {
-    return this.fs.write(this.destinationPath(DESTINATION_MODEL_SMOKE), Util.getRequireStatement(fileName))
+  if (!this.fs.exists(this.destinationPath(DESTINATION_MODEL_SMOKE(name)))) {
+    return this.fs.write(this.destinationPath(DESTINATION_MODEL_SMOKE(name)), Util.getRequireStatement(fileName))
   }
 };
