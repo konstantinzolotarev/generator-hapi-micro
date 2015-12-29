5f45517f94c8e9541d1b3af4e5ce53b929cce301
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2015-12-29 00:31:57 -0500

    fix

--- src/model-test/steps/writing.js
+++ src/model-test/steps/writing.js
@@ -2,7 +2,8 @@
  * Step 5
  * Where you write the generator specific files (routes, controllers, etc)
  */
-
+ import chalk from 'chalk';
+ import printMessage from 'print-message';
 
 const SOURCE_SMOKE_MODEL = `Model.smoke.js`;
 const SOURCE_INTEGRATION_MODEL = `Model.integration.js`;
@@ -12,14 +13,22 @@ const DESTINATION_MODEL_SMOKE = name => `test/smoke/models/${name}.test.js`;
 const DESTINATION_MODEL_INTEGRATION = name => `test/integration/models/${name}.test.js`;
 const DESTINATION_MODEL_HELPER = name => `test/helper/model/${name}.js`;
 
-export default function () {
+export default {
 
-  let name = (this['model-name'].charAt(0).toUpperCase() + this['model-name'].slice(1)).replace(/(\w+)Model$/, '$1');
-  let fileName = `${name}`
+  smoke() {
+    let name = (this['model-name'].charAt(0).toUpperCase() + this['model-name'].slice(1)).replace(/(\w+)Model$/, '$1');
+    let fileName = `${name}`
 
-  this.template(SOURCE_SMOKE_MODEL, DESTINATION_MODEL_SMOKE(name), {name, fileName, answers: this.answers});
+    this.template(SOURCE_SMOKE_MODEL, DESTINATION_MODEL_SMOKE(name), {name, fileName, answers: this.answers});
 
-  if (!this.fs.exists(this.destinationPath(DESTINATION_MODEL_SMOKE(name)))) {
-    return this.fs.write(this.destinationPath(DESTINATION_MODEL_SMOKE(name)), Util.getRequireStatement(fileName))
+    if (!this.fs.exists(this.destinationPath(DESTINATION_MODEL_SMOKE(name)))) {
+      return this.fs.write(this.destinationPath(DESTINATION_MODEL_SMOKE(name)), Util.getRequireStatement(fileName))
+    } else {
+      printMessage([
+        `Your test file: ${chalk.green(this.destinationPath(DESTINATION_MODEL_SMOKE(name)))} already exist!`,
+      ], {
+        printFn: this.log
+      });
+    }
   }
 };
